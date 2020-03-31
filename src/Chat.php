df1eca44b5e9d2c1afbe189284371fb26b9bbaff
Pierre MINIGGIO <pierre@miniggiodev.fr>
2020-03-31 22:37:40 +0200

    real time chat

--- src/Chat.php
+++ src/Chat.php
@@ -9,7 +9,7 @@ use SplObjectStorage;
 
 class Chat implements MessageComponentInterface
 {
-    
+
     protected SplObjectStorage $clients;
 
     public function __construct()
@@ -19,13 +19,22 @@ class Chat implements MessageComponentInterface
 
     public function onOpen(ConnectionInterface $conn): void
     {
+        // Store the new connection to send messages to later
         $this->clients->attach($conn);
+
+        echo "New connection! ({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg): void
     {
+        $numRecv = count($this->clients) - 1;
+
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv === 1 ? '' : 's');
+
         foreach ($this->clients as $client) {
-            if ($from != $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
                 $client->send($msg);
             }
         }
@@ -33,11 +42,16 @@ class Chat implements MessageComponentInterface
 
     public function onClose(ConnectionInterface $conn): void
     {
+        // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
+
+        echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
     public function onError(ConnectionInterface $conn, Exception $e): void
     {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
         $conn->close();
     }
 }
